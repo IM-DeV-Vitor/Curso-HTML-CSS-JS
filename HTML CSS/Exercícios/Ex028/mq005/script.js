05f27f3f926e94d7f35e8ca713576c0e1496a0c0
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-03-22 14:27:02 -0400

    menu hamburger yee

---
+++ HTML CSS/Exercícios/Ex028/mq005/script.js
@@ -0,0 +1,8 @@
+function click_menu(){
+    var menu = document.getElementById("items")
+    if (menu.style.display == "block") {
+        menu.style.display = "none"
+    } else {
+        menu.style.display = "block"
+    }
+}
